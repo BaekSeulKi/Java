258c0b858139146cc1caca70472b39e1a5939152
baekyeseul <201802103@o.cnu.ac.kr>
2022-03-18 15:14:29 +0900

    baekjoon_java

---
+++ java_10172.java
@@ -0,0 +1,12 @@
+package baekjoon;
+import java.io.*;
+public class java_10172 {
+	 public static void main(String[] args) throws IOException{
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	        bw.write("|\\_/|"+"\n"+"|q p|   /}"+"\n"+"( 0 )\"\"\"\\"+"\n"+"|\"^\"`    |"+"\n"+"||_/=\\\\__|");
+	        bw.flush();
+	        bw.close();
+	        
+	    }
+
+}
